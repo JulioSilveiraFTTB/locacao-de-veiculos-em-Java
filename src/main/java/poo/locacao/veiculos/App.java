072848a002dd71b6c4f5867779bb8c13a6bfd331
Júlio Silveira <julio.silveira@ayvu.net>
2022-10-18 21:09:49 -0300

    testando métodos da classe Veiculos

--- src/main/java/poo/locacao/veiculos/App.java
+++ src/main/java/poo/locacao/veiculos/App.java
@@ -8,6 +8,8 @@ import poo.locacao.veiculos.objetos.tipoveiculo.enums.ArCondicionado;
 import poo.locacao.veiculos.objetos.tipoveiculo.enums.Categoria;
 import poo.locacao.veiculos.objetos.tipoveiculo.enums.Wifi;
 
+import poo.locacao.veiculos.objetos.arraylists.*;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -15,32 +17,38 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Teste... \n" );
 
-        List<Veiculo> listaDeVeiculos = new ArrayList<>();
-        List<Cliente> listaDeClientes = new ArrayList<>();
+        Veiculos v = new Veiculos();
+
+        // List<Veiculo> listaDeVeiculos = new ArrayList<>();
+        // List<Cliente> listaDeClientes = new ArrayList<>();
         
         Veiculo carro = new Carro("ABC", 2010, 10.0, 4, 2, 10.0, ArCondicionado.SIM);
-        listaDeVeiculos.add(carro);
-        Veiculo carro2 = new Carro("DEF", 2020, 20.0, 4, 4, 15.0, ArCondicionado.NAO);
-        listaDeVeiculos.add(carro2);
-        Veiculo onibus = new Onibus("GHI", 2005, 500.0, 25, Categoria.CONVECIONAL, ArCondicionado.NAO, Wifi.NAO);
-        listaDeVeiculos.add(onibus);
+        v.add(carro);
+        System.out.println(v.getVeiculos());
+        v.remove("ABC");
 
-        System.out.println(listaDeVeiculos + "\n");
+        // Veiculo carro2 = new Carro("DEF", 2020, 20.0, 4, 4, 15.0, ArCondicionado.NAO);
+        // listaDeVeiculos.add(carro2);
+        // Veiculo onibus = new Onibus("GHI", 2005, 500.0, 25, Categoria.CONVECIONAL, ArCondicionado.NAO, Wifi.NAO);
+        // listaDeVeiculos.add(onibus);
 
-        Cliente cliente = new Cliente("Julio", "Rua Sete", 100055500, 1012202201, 99999999);
-        listaDeClientes.add(cliente);
-        Cliente cliente2 = new Cliente("Guilherme", "Rua Dois", 100042000, 1012202202, 99999998);
-        listaDeClientes.add(cliente2);
-        Cliente cliente3 = new Cliente("Alguem", "Rua Dez", 100030000, 1012202203, 99999997);
-        listaDeClientes.add(cliente3);
+        // // System.out.println(listaDeVeiculos + "\n");
 
-        System.out.println(listaDeClientes);
+        // Cliente cliente = new Cliente("Julio", "Rua Sete", 100055500, 1012202201, 99999999);
+        // listaDeClientes.add(cliente);
+        // Cliente cliente2 = new Cliente("Guilherme", "Rua Dois", 100042000, 1012202202, 99999998);
+        // listaDeClientes.add(cliente2);
+        // Cliente cliente3 = new Cliente("Alguem", "Rua Dez", 100030000, 1012202203, 99999997);
+        // listaDeClientes.add(cliente3);
 
-        carro.setPlaca("LOL");
-        carro.setAno(2022);
-        carro.setDiaria(50.0);
-        listaDeVeiculos.remove(carro2);
-        listaDeVeiculos.remove(onibus);
-        System.out.println(listaDeVeiculos + "\n");
+        // System.out.println(listaDeClientes);
+
+        // carro.setPlaca("LOL");
+        // carro.setAno(2022);
+        // carro.setDiaria(50.0);
+        // listaDeVeiculos.remove(carro2);
+        // listaDeVeiculos.remove(onibus);
+        // System.out.println(listaDeVeiculos + "\n");
+        System.out.println(v.getVeiculos());
     }
 }
