e8272721f97db4366baa1fb4e6a5b248b422ef1b
Rodrigo Rodrigues <rodrigo.rodrigues777@hotmail.com>
2022-10-20 21:55:16 -0300

    Acrescentado metódo de adição do carro na lista de veiculos

--- src/main/java/poo/locacao/veiculos/menus/MenuVeiculo.java
+++ src/main/java/poo/locacao/veiculos/menus/MenuVeiculo.java
@@ -2,32 +2,34 @@ package poo.locacao.veiculos.menus;
 
 import poo.locacao.veiculos.objetos.arraylists.*;
 import poo.locacao.veiculos.objetos.Cor;
+import poo.locacao.veiculos.objetos.tipoveiculo.Carro;
+import poo.locacao.veiculos.objetos.tipoveiculo.enums.ArCondicionado;
 import poo.locacao.veiculos.objetos.tipoveiculo.enums.Color;
 import poo.locacao.veiculos.Menu;
+import poo.locacao.veiculos.objetos.Veiculo;
 
 import java.util.Scanner;
 
 public class MenuVeiculo {
-    Veiculos v = new Veiculos();
-
+    private static Veiculos lV = new Veiculos();
+    private static Veiculo v;
+    private static Scanner in = new Scanner(System.in);
+    
     public static void areaVeiculo() {
        int op;
-       boolean repeat;
-       boolean defaultRepeat;
-       // menu com as op��es dos m�todos
-       Cor.printf(Color.GREEN, "[VEICULOS - LOCADORA DE VEICULOS]\n\n");
-       Cor.printf("    1 - Cadastrar novo veiculo\n");
-       Cor.printf("    2 - Consultar veiculo (por placa)\n");
-       Cor.printf("    3 - Consultar veiculos (por placa/resumido)\n");
-       Cor.printf("    4 - Consultar lista completa de veiculos\n");
-       Cor.printf("    5 - Remover veiculo\n");
-       Cor.printf("    6 - Retornar ao menu principal\n");
-       Cor.printf("    0 - Sair\n");
-       Cor.printf(Color.BLUE, "\nDigite uma das opcoes acima: ");
-   
-       try (Scanner in = new Scanner(System.in)) {
+       
         do {
-            defaultRepeat = repeat = false;
+        	// menu com as op��es dos m�todos
+            Cor.printf(Color.GREEN, "[VEICULOS - LOCADORA DE VEICULOS]\n\n");
+            Cor.printf("    1 - Cadastrar novo veiculo\n");
+            Cor.printf("    2 - Consultar veiculo (por placa)\n");
+            Cor.printf("    3 - Consultar veiculos (por placa/resumido)\n");
+            Cor.printf("    4 - Consultar lista completa de veiculos\n");
+            Cor.printf("    5 - Remover veiculo\n");
+            Cor.printf("    6 - Retornar ao menu principal\n");
+            Cor.printf("    0 - Sair\n");
+            Cor.printf(Color.BLUE, "\nDigite uma das opcoes acima: ");
+        
             op = in.nextInt();
             in.nextLine();
 
@@ -60,11 +62,86 @@ public class MenuVeiculo {
                     Menu.menuInicial();
                     break;
                 }
-        } while (repeat || defaultRepeat);
-       }
+        } while (op != 0);
+       
     }
 
     public static void cadastroVeiculo() {
+    	int op;
+    	int op2;
+        String placa;
+        int ano;
+        double diaria;
+    	
+        do {
+        	//menu com as opcoes de tipo de veiculo
+            Cor.printf(Color.GREEN, "[VEICULOS - LOCADORA DE VEICULOS]\n\n");
+            Cor.printf("\t Informe o tipo de veiculo que deseja cadastrar:");
+            Cor.printf("\t 1 - Cadastrar carro\n");
+            Cor.printf("\t 2 - Cadastrar onibus\n");
+            Cor.printf("\t 3 - Cadastrar casminhao");
+            Cor.printf("\t 0 - Sair\n");
+            Cor.printf(Color.BLUE, "\n Digite uma das opcoes acima: ");
+            op = in.nextInt();
+            in.nextLine();
+            
+            switch(op) {
+            case 1:
+            	int nPassageiros;
+            	int nPortas;
+            	double consumo;
+            	ArCondicionado arCond;
+            	Cor.printf("\tInforme os dados do carro\n");
+            	Cor.printf("Placa:");
+            	placa = in.nextLine();
+            	
+            	Cor.printf("Ano: ");
+            	ano = in.nextInt();
+            	in.nextLine();
+            	
+            	Cor.printf("Valor da diaria: ");
+            	diaria = in.nextDouble();
+            	in.nextLine();
+            	
+            	Cor.printf("Numero de passageiros: ");
+            	nPassageiros = in.nextInt();
+            	in.nextLine();
+            	
+            	Cor.printf("Numero de portas: ");
+            	nPortas = in.nextInt();
+            	in.nextLine();
+            	
+            	Cor.printf("Consumo: ");
+            	consumo = in.nextDouble();
+            	in.nextLine();
+            	
+            	Cor.printf("Possui ar condicionado?\n1 - SIM\n2 - NÃO\n");
+            	
+            	Cor.printf("Informe sua opção: ");
+            	op2 = in.nextInt();
+            	in.nextLine();
+            	
+            	if(op2 == 1)
+            		arCond = ArCondicionado.SIM;
+            	else
+            		arCond = ArCondicionado.NAO;
+            	
+            	v = new Carro(placa, ano, diaria, nPassageiros, nPortas,consumo, arCond);
+            	
+            	lV.add(v);
+            	
+            	break;
+            case 2:
+            	
+            	break;
+            case 3:
+            	
+            	break;
+            }
+            
+    	}while(op != 0);
+        
+        
         
     }
 
